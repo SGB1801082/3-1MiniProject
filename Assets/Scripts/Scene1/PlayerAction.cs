fbdaadcb51929dc2b0bc99d884833b9581c1d4f2
SGB1801082 <145640640+SGB1801082@users.noreply.github.com>
2024-08-03 02:30:49 +0900

    플레이어 애니메이션 수정

--- Assets/Scripts/Scene1/PlayerAction.cs
+++ Assets/Scripts/Scene1/PlayerAction.cs
@@ -1,153 +1,107 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerAction : MonoBehaviour
 {
-    //1차
-    [SerializeField] private float speed;
-    private float h, v;
-    Rigidbody2D rigid;
+    public Rigidbody2D rigid;
+    public Animator animator;
 
-    //2차
-    private bool isHorizontalMove;
-    private Animator animator;
-    private Vector3 dirVec;// Ray를 발사하고 충돌을 체크할때 사용할, 현재 바라보고 있는 방향 값을 가진 변수 선언 
-    private GameObject scanRayObjcet;// Ray에 충돌한 오브젝트를 저장할 변수
+    float h, v;
+    Vector2 moveDirection;
 
-    //3차
-    public GameUiMgr gUiMgr;
+    private bool prevIsMovingLeft, prevIsMovingRight, prevIsMovingUp, prevIsMovingDown, prevIsIdle;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         animator = GetComponentInChildren<Animator>();
     }
-    private void Update()
+
+    void Update()
     {
-        // Move Value //플레이어가 대화 상호작용 중일때에는 캐릭터가 움직이지 못 하도록 isActionTalk값이 없을때만 움직일 수 있도록 코드 수정
-        h = gUiMgr.isActionTalk ? 0 : Input.GetAxisRaw("Horizontal");
-        v = gUiMgr.isActionTalk ? 0 : Input.GetAxisRaw("Vertical");
+        // 입력 값을 확인
+        bool left = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+        bool right = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+        bool up = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
+        bool down = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
 
-        // Check Button Down & Up //플레이어가 대화 상호작용 중일때에는 캐릭터가 움직이지 못 하도록 isActionTalk값이 없을때만 움직일 수 있도록 코드 수정
-        bool hDown = gUiMgr.isActionTalk ? false : Input.GetButtonDown("Horizontal");// 오른쪽
-        bool vDown = gUiMgr.isActionTalk ? false : Input.GetButtonDown("Vertical");// 아래
-        bool hUp = gUiMgr.isActionTalk ? false : Input.GetButtonDown("Horizontal");// 왼쪽
-        bool vUp = gUiMgr.isActionTalk ? false : Input.GetButtonDown("Vertical");// 위
+        // 이동 방향 설정
+        h = (right ? 1 : 0) - (left ? 1 : 0);
+        v = (up ? 1 : 0) - (down ? 1 : 0);
 
-        // Check Horizontal Move
-        if (hDown)
-        {
-            isHorizontalMove = true;
-        }
-        else if (vDown)
-        {
-            isHorizontalMove = false;
-        }
-        else if (hUp || vUp) // 양쪽(ex +h, -h) 버튼을 누른 상태로 한 쪽만 버튼 업(+h)이면 발생하는 문제를 해결하기위함
-        {
-            isHorizontalMove = (h != 0);// 현재 속도를 측정하여 체크함
-        }
-
-        //Animation
-        if (animator.GetInteger("hAxisRaw") != h)
-        {
-            animator.SetInteger("hAxisRaw", (int)h);
-            animator.SetBool("isChange", true);
-        }
-        else if (animator.GetInteger("vAxisRaw") != v)
-        {
-            animator.SetBool("isChange", true);
-            animator.SetInteger("vAxisRaw", (int)v);
-        }
-        else
-        {
-            animator.SetBool("isChange", false);
-        }
-
-
-        //Direction == 플레이어가 바라보는 방향
-        // 특정 방향으로 이동중, 키를 떼지않고 다른방향으로 이동하다가 키를 떼고, 특정방향으로 계속 움직일 경우 레이가 잠시 이동했던 방향으로 일시적으로 고정되는 현상 발생 03-10
-        if (vDown && v > 0) // 위를 보고있을때
-        {
-            dirVec = Vector3.up;// 레이저 위로 발사
-        }
-        else if (vDown && v < 0) // 아래를 보고있을때
-        {
-            dirVec = Vector3.down;
-        }
-        else if (hDown && h < 0) // 왼쪽를 보고있을때
-        {
-            dirVec = Vector3.left;
-        }
-        else if (hDown && h > 0) // 오른쪽를 보고있을때
-        {
-            dirVec = Vector3.right;
-        }
-
-
-        // Scan Ray Object
-        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) && scanRayObjcet != null || (GameUiMgr.single.isActionTalk && Input.GetMouseButtonDown(0)))
-        {
-            //Debug.Log("This is : "+ scanRayObjcet.name);// 레이에 맞고 스페이스바를 통해 상호작용하여 Scan Object에 저장된 물체가 있을 시 오브젝트 이름 출력 
-            //Debug.Log(scanRayObjcet.name); // 04- 23 Debug
-            
-            ObjectData obj = scanRayObjcet.GetComponent<ObjectData>();
-            if (obj.id == 9000)
-            {
-                if (GameUiMgr.single.questMgr.questId >= 30)
-                {
-                    GameUiMgr.single.panelPartyBoard.SetActive(true);
-                    Debug.Log("id: " + obj.id);
-                    return;
-                }
-                else
-                    return;
-            }
-            else if (obj.id == 8000)
-            {
-                Debug.Log("8000 실행");
-                if (GameUiMgr.single.questMgr.questId >= 40)
-                {
-                    GameUiMgr.single.isDungeon = true;
-                    GameUiMgr.single.textEquipPanel.text = "던전에 입장하시겠습니까?";//OK버튼 클릭했을때 다른효과가 나와야하는데 생각조금 더 해봐야함
-                    GameUiMgr.single.addEquipPanel.gameObject.SetActive(true);
-                    return;
-                }
-                else
-                {
-                    // ToDo: CallBack Img.SetActive(true);
-                    Debug.Log("튜토리얼 던전에 진입할 수 없습니다.");
-                    return;
-                }
-            }
-
-            gUiMgr.TalkAction(scanRayObjcet);// Ray로 상호작용한 Object의 정보를 twonMgr로 넘겨서 그곳에 있는 대화창에 정보를 출력하게 함.
-        }
-
+        // 애니메이션 상태 업데이트
+        UpdateAnimation();
     }
 
     private void FixedUpdate()
     {
-        Vector2 moveVec = isHorizontalMove ? new Vector2(h, 0) : new Vector2(0, v);
-        //rigid.velocity = new Vector2(h, v) * speed;
-        rigid.velocity = moveVec * speed;
-
-        // 다른 오브젝트와의 충돌을 체크하기위한 Ray
-        Debug.DrawRay(rigid.position, dirVec * 0.8f, new Color(0, 1, 0));// DrawRay (Scene창에서만 보임)를 발사하는데, 현재 캐릭터의 위치값, 길이(float), 색갈(new Color)이 필요
-        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 0.8f, LayerMask.GetMask("Object"));// 마스크가 Object인 경우에만 충돌(이미 플레이어가 사용중인 콜라이더와의 충돌을 피하기 위함.)
-
-        // 레이가 오브젝트와 충돌할 시 ScanObject에 저장 
-        if (rayHit.collider != null)
+        moveDirection = new Vector2(h, v).normalized;
+        if (!GameUiMgr.single.isActionTalk)
         {
-            scanRayObjcet = rayHit.collider.gameObject;// RayCast된 오브젝트를 변수로 저장하여 활용 할 수 있게함
+            rigid.velocity = moveDirection * 4f;
         }
-        else
-        {
-            scanRayObjcet = null;// 충돌이없다면 scanObj는 비워져야함.
-        }
-
     }
 
+    void UpdateAnimation()
+    {
+        bool isMovingLeft = h < 0;
+        bool isMovingRight = h > 0;
+        bool isMovingUp = v > 0;
+        bool isMovingDown = v < 0;
+        bool isIdle = !isMovingLeft && !isMovingRight && !isMovingUp && !isMovingDown;
+
+        // 애니메이터의 파라미터 업데이트
+        animator.SetBool("IsMovingLeft", isMovingLeft);
+        animator.SetBool("IsMovingRight", isMovingRight);
+        animator.SetBool("IsMovingUp", isMovingUp);
+        animator.SetBool("IsMovingDown", isMovingDown);
+        animator.SetBool("IsIdle", isIdle);
+
+        // 애니메이션 상태에 따라 캐릭터의 이동을 조정
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        if (stateInfo.IsName("Player_Left_Walk"))
+        {
+            // Left 애니메이션이 실행 중일 때
+            h = -1; // 왼쪽 이동
+            v = 0;  // 상하 이동 없음
+        }
+        else if (stateInfo.IsName("Player_Right_Walk"))
+        {
+            // Right 애니메이션이 실행 중일 때
+            h = 1; // 오른쪽 이동
+            v = 0;  // 상하 이동 없음
+        }
+        else if (stateInfo.IsName("Player_Up_Walk"))
+        {
+            // Up 애니메이션이 실행 중일 때
+            h = 0; // 좌우 이동 없음
+            v = 1; // 위쪽 이동
+        }
+        else if (stateInfo.IsName("Player_Down_Walk"))
+        {
+            // Down 애니메이션이 실행 중일 때
+            h = 0; // 좌우 이동 없음
+            v = -1; // 아래쪽 이동
+        }
+        else if (stateInfo.IsName("Player_Idle"))
+        {
+            // Idle 애니메이션이 실행 중일 때
+            h = 0;
+            v = 0;
+        }
+
+        // 디버그 로그 출력
+        if (isMovingLeft != prevIsMovingLeft || isMovingRight != prevIsMovingRight ||
+            isMovingUp != prevIsMovingUp || isMovingDown != prevIsMovingDown || isIdle != prevIsIdle)
+        {
+            Debug.Log($"Left: {isMovingLeft}, Right: {isMovingRight}, Up: {isMovingUp}, Down: {isMovingDown}, Idle: {isIdle}");
+        }
+
+        // 이전 상태 업데이트
+        prevIsMovingLeft = isMovingLeft;
+        prevIsMovingRight = isMovingRight;
+        prevIsMovingUp = isMovingUp;
+        prevIsMovingDown = isMovingDown;
+        prevIsIdle = isIdle;
+    }
 }
